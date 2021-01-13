539ab646d421e51ace0261e54af0f899b7ae2663
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-13 16:34:50 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  # Userモデルに各カラムのバリデーションを記述（「emailとpasswordが空だと保存できない」というバリデーションは標準で用意されているため、記述する必要はない）
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 end
