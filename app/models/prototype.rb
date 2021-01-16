505b4ce38ddb40c6b9bf8db67fd5daee7b54e396
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-16 15:43:23 +0900

    バリデーション設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,4 +3,13 @@ class Prototype < ApplicationRecord
   belongs_to :user
   # has_one_attachedを使用してimageカラムとのアソシエーションを記述
   has_one_attached :image
+
+
+  # Prototypeモデルに、プロトタイプの名称、キャッチコピー、コンセプト、画像に関するバリデーションを記述した
+  with_options presence: true do
+    validates :title
+    validates :catch_copy
+    validates :concept
+    validates :image
+  end
 end
