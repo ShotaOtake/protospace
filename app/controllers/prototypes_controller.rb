1a5f31820ad606441c02092e7a8e26d7b86bae23
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-16 18:07:05 +0900

    newアクション編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,7 +3,9 @@ class PrototypesController < ApplicationController
   end
 
   # prototypesコントローラーにnewアクションとcreateアクションを設定
+  # newアクションにインスタンス変数@prototypeを定義し、Prototypeモデルの新規オブジェクトを代入
   def new
+    @prototype = Prototype.new
   end
 
   def create
