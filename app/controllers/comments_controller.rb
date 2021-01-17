34c36b59e50baa953b799ac295b4c1addd13a035
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-17 21:36:07 +0900

    ストロングパラメーター

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,12 @@ class CommentsController < ApplicationController
   def create
   end
 
+
+  private
+# commentsコントローラーのprivateメソッドにストロングパラメーターをセットし、特定の値のみを受け付けるようにした。
+# 且つ、user_idとprototype_idもmergeした
+  def comment_params
+    params.require(:comment).permit(:text).merge( user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
 end
