53bf782c32c99aa8d4884e33d13be89a48e93655
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-17 16:15:11 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'prototypes#index'
   # resourcesを用いてnewアクションとcreateアクションに対するルーティングを記述
   # resourcesを用いてshowアクションに対するルーティングを記述
-  resources :prototypes, only: [:new, :create, :show, :edit, :update] do
+  # resourcesを用いてdestroyアクションに対するルーティングを記述
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
   end
 end
